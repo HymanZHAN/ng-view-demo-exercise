650c0ca400bd859de638f6c77c020e0e9fcce542
Xucong Zhan <xzhan@connect.ust.hk>
2023-04-10 20:29:55 +0800

    feat: vcr create component

--- src/app/features/view-container-ref/create-component/create-component.component.ts
+++ src/app/features/view-container-ref/create-component/create-component.component.ts
@@ -1,4 +1,12 @@
-import { ChangeDetectionStrategy, Component } from "@angular/core";
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ComponentRef,
+  OnDestroy,
+  Renderer2,
+  ViewChild,
+  ViewContainerRef,
+} from "@angular/core";
 import { CommonModule } from "@angular/common";
 import {
   ArrowDownOutlineComponent,
@@ -6,6 +14,8 @@ import {
   PlusOutlineComponent,
   TrashOutlineComponent,
 } from "@components/icons";
+import { CardComponent } from "@components/card/card.component";
+import { PicCardComponent } from "@components/card/pic-card.component";
 
 @Component({
   selector: "app-create-component",
@@ -27,4 +37,80 @@ import {
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class CreateComponentComponent {}
+export default class CreateComponentComponent implements OnDestroy {
+  @ViewChild("container", { read: ViewContainerRef })
+  private container!: ViewContainerRef;
+
+  private count = 0;
+  private cards: ComponentRef<CardComponent | PicCardComponent>[] = [];
+  private selectedCard?: ComponentRef<CardComponent | PicCardComponent>;
+  private unlisteners: (() => void)[] = [];
+
+  constructor(private renderer: Renderer2) {}
+
+  ngOnDestroy(): void {
+    this.unlisteners.forEach((f) => f());
+  }
+
+  createNewCard(index?: number) {
+    const comp = this.container.createComponent(CardComponent, {
+      index,
+      projectableNodes: this.getNodesForCard(),
+    });
+
+    this.setupCardCmp(comp);
+  }
+
+  createNewPicCard(index?: number) {
+    const comp = this.container.createComponent(PicCardComponent, {
+      index,
+    });
+
+    this.setupCardCmp(comp);
+  }
+
+  private setupCardCmp(cmpRef: ComponentRef<CardComponent | PicCardComponent>) {
+    cmpRef.setInput("index", ++this.count);
+    this.cards.push(cmpRef);
+
+    const unlisten = this.renderer.listen(
+      cmpRef.location.nativeElement,
+      "click",
+      () => {
+        // set other cards to unselected
+        this.cards.forEach((c) => c.setInput("selected", false));
+        // set this card to selected
+        cmpRef.setInput("selected", true);
+        // sync UI state
+        cmpRef.changeDetectorRef.markForCheck();
+
+        this.selectedCard = cmpRef;
+      }
+    );
+    this.unlisteners.push(unlisten);
+  }
+
+  removeCard() {
+    if (this.selectedCard) {
+      this.cards = this.cards.filter((c) => c !== this.selectedCard);
+      const selectedCardViewRef = this.selectedCard.hostView;
+      this.container.remove(this.container.indexOf(selectedCardViewRef));
+    }
+  }
+
+  getNodesForCard(): Node[][] {
+    const titleNode: HTMLParagraphElement = this.renderer.createElement("p");
+    this.renderer.setProperty(titleNode, "innerText", "New Card");
+    this.renderer.setAttribute(titleNode, "title", "");
+
+    const bodyNode: HTMLParagraphElement = this.renderer.createElement("p");
+    this.renderer.setProperty(
+      bodyNode,
+      "innerHTML",
+      "This is created by <code>createComponent</code>"
+    );
+    this.renderer.setAttribute(bodyNode, "body", "");
+
+    return [[titleNode], [bodyNode]];
+  }
+}
