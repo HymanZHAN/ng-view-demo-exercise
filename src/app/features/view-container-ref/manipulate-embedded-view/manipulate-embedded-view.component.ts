019116a8b0c6be953cf50b49a541c3135b221830
Xucong Zhan <xzhan@connect.ust.hk>
2023-04-14 21:36:14 +0800

    feat: vcr manipulate embedded view

--- src/app/features/view-container-ref/manipulate-embedded-view/manipulate-embedded-view.component.ts
+++ src/app/features/view-container-ref/manipulate-embedded-view/manipulate-embedded-view.component.ts
@@ -1,4 +1,11 @@
-import { ChangeDetectionStrategy, Component } from "@angular/core";
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EmbeddedViewRef,
+  TemplateRef,
+  ViewChild,
+  ViewContainerRef,
+} from "@angular/core";
 import { CommonModule } from "@angular/common";
 import {
   PlusOutlineComponent,
@@ -29,4 +36,66 @@ import {
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class ManipulateEmbeddedViewComponent {}
+export default class ManipulateEmbeddedViewComponent {
+  @ViewChild("container", { read: ViewContainerRef })
+  private container!: ViewContainerRef;
+
+  @ViewChild("listItemTemplate", { read: TemplateRef })
+  private itemTemplate!: TemplateRef<unknown>;
+
+  private counter = 0;
+  private selectedItemView?: EmbeddedViewRef<unknown>;
+  private nameToViews: Map<string, EmbeddedViewRef<unknown>> = new Map();
+  public selectedItemName: string = "";
+
+  addItem() {
+    const name = `Item ${++this.counter}`;
+    const itemView = this.container.createEmbeddedView(this.itemTemplate, {
+      $implicit: name,
+    });
+    this.nameToViews.set(name, itemView);
+  }
+
+  selectItem(name: string) {
+    this.selectedItemName = name;
+    this.selectedItemView = this.nameToViews.get(name);
+  }
+
+  removeItem() {
+    if (this.selectedItemView) {
+      const index = this.container.indexOf(this.selectedItemView);
+      this.container.remove(index);
+      this.nameToViews.delete(this.selectedItemName);
+    }
+  }
+
+  moveItemDown() {
+    if (this.selectedItemView) {
+      const currentIndex = this.container.indexOf(this.selectedItemView);
+      this.container.move(
+        this.selectedItemView,
+        this.container.length > currentIndex + 1
+          ? currentIndex + 1
+          : currentIndex
+      );
+    }
+  }
+
+  moveItemUp() {
+    if (this.selectedItemView) {
+      const currentIndex = this.container.indexOf(this.selectedItemView);
+      this.container.move(
+        this.selectedItemView,
+        currentIndex === 0 ? currentIndex : currentIndex - 1
+      );
+    }
+  }
+
+  reset() {
+    this.container.clear();
+    this.counter = 0;
+    this.selectedItemView = undefined;
+    this.nameToViews = new Map();
+    this.selectedItemName = "";
+  }
+}
