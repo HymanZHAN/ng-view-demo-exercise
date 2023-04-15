93ee466f3378d5b15033ea9c446a3f3ab7f4815e
Xucong Zhan <xzhan@connect.ust.hk>
2023-04-15 21:58:53 +0800

    feat: wrap up reuse views

--- src/app/features/view-container-ref/reuse-views/reuse-views.component.ts
+++ src/app/features/view-container-ref/reuse-views/reuse-views.component.ts
@@ -1,4 +1,12 @@
-import { ChangeDetectionStrategy, Component } from "@angular/core";
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EmbeddedViewRef,
+  TemplateRef,
+  ViewChild,
+  ViewContainerRef,
+  ViewRef,
+} from "@angular/core";
 import { CommonModule } from "@angular/common";
 import {
   ArrowDownOutlineComponent,
@@ -31,4 +39,60 @@ import { ArrowUturnLeftOutlineComponent } from "@components/icons/arrow-uturn-le
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class ReuseViewsComponent {}
+export default class ReuseViewsComponent {
+  @ViewChild("container", { read: ViewContainerRef })
+  private container!: ViewContainerRef;
+
+  @ViewChild("listItemTemplate", { read: TemplateRef })
+  private itemTemplate!: TemplateRef<unknown>;
+
+  private counter = 0;
+  private selectedItemView?: EmbeddedViewRef<unknown>;
+  private nameToViews: Map<string, EmbeddedViewRef<unknown>> = new Map();
+  public selectedItemName: string = "";
+  private detachedItem: ViewRef | null = null;
+
+  selectItem(name: string) {
+    this.selectedItemName = name;
+    this.selectedItemView = this.nameToViews.get(name);
+  }
+
+  addItem() {
+    const name = `Item ${++this.counter}`;
+    const embeddedView = this.container.createEmbeddedView(this.itemTemplate, {
+      $implicit: name,
+    });
+    this.nameToViews.set(name, embeddedView);
+  }
+
+  removeItem() {
+    if (this.selectedItemView) {
+      const index = this.container.indexOf(this.selectedItemView);
+      this.container.remove(index);
+      this.nameToViews.delete(this.selectedItemName);
+    }
+  }
+
+  detachItem() {
+    if (this.selectedItemView) {
+      const index = this.container.indexOf(this.selectedItemView);
+      this.detachedItem =  this.container.detach(index);
+    }
+  }
+
+  insertItem() {
+    if (this.selectedItemView && this.detachedItem) {
+      const index = this.container.indexOf(this.selectedItemView);
+      this.container.insert(this.detachedItem, index);
+      this.detachedItem = null;
+    }
+  }
+
+  reset() {
+    this.container.clear();
+    this.counter = 0;
+    this.selectedItemView = undefined;
+    this.nameToViews = new Map();
+    this.selectedItemName = "";
+  }
+}
